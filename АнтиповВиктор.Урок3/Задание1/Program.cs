b677eaa1b6a961283c0376ca33414df973bd8eb3
Victor Antipov <antipov-victor1998@yandex.ru>
2020-06-09 16:22:59 +0300

    Урок3

---
+++ АнтиповВиктор.Урок3/Задание1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание1
+{
+    /* С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке).
+     * Требуется подсчитать сумму всех нечётных положительных чисел.
+     * Сами числа и сумму вывести на экран, используя tryParse.*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = number();
+            int sum = 0;
+            while(num != 0)
+            {
+                if (num > 0)
+                {
+                    if (num % 2==1)
+                    {
+                        sum = sum + num;
+                    }
+                }
+                num = number();
+            }
+            Console.WriteLine($"Сумма положительных нечётных чисел = {sum}");
+
+            Console.ReadKey();
+
+        }
+
+        static int number()
+        {
+            Console.Write("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            return num;
+        }
+    }
+}
