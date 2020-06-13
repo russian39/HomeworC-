6a7f308325270bd0e4b3de3b7c9f5dadefd1dec9
Victor Antipov <antipov-victor1998@yandex.ru>
2020-06-13 12:06:53 +0300

    Урок4

---
+++ АнтиповВиктор.Урок4/Задание1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание1
+{
+    /*Дан целочисленный массив из 20 элементов.
+     * Элементы массива могут принимать целые значения от –10000 до 10000 включительно. Заполнить случайными числами.
+     * Написать программу, позволяющую найти и вывести количество пар элементов массива,
+     * в которых только одно число делится на 3.
+     * В данной задаче под парой подразумевается два подряд идущих элемента массива.
+     * Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2.*/
+    //Антипов
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rnd = new Random();
+            int couples = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-10000, 10000);
+                Console.WriteLine(array[i]);
+                if (array[i]%3==0)
+                {
+                    couples++;
+                }
+            }
+            couples = couples / 2;
+            Console.WriteLine(couples);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ АнтиповВиктор.Урок4/Задание4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание4
+{
+    /*Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив.
+     * Создайте структуру Account, содержащую Login и Password.*/
+    //Антипов
+
+    struct Account
+    {
+        string[] fileArray;
+        string login;
+        string password;
+        public Account(string start)
+        {
+            fileArray = File.ReadAllLines("C:\\Users\\Юрий Фалин\\source\\repos\\АнтиповВиктор.Урок4\\Задание4\\ВходныеДанные.txt");
+            login = fileArray[0];
+            password = fileArray[1];
+        }
+
+        public string trueLogin() 
+        {
+            return login;
+        }
+
+        public string truePassword()
+        {
+            return password;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account("Данные аккаунта");
+
+            string login;
+            string password;
+            int i = 1;
+
+            do
+            {
+                Console.Write("Логин: ");
+                login = Console.ReadLine();
+
+                Console.Write("Пароль: ");
+                password = Console.ReadLine();
+
+                if (login == acc.trueLogin() && password == acc.truePassword())
+                {
+                    Console.WriteLine("Добро пожаловать!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный логин или пароль. Повторите попытку.\nОсталось {3 - i} попыток");
+                    i++;
+                }
+            }
+            while (i < 4);
+
+            if (i == 4)
+            {
+                Console.WriteLine("Количество попыток входа превысило лимит. Повторите попытку позже.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
