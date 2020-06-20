9b546ef80e4652d95a9d02172ff72ff25f382863
Victor Antipov <antipov-victor1998@yandex.ru>
2020-06-20 12:54:17 +0300

    Урок6

---
+++ АнтиповВиктор.Урок6/Задание3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание3
+{
+    class Student
+    {
+        public string lastName;
+        public string firstName;
+        public string university;
+        public string faculty;
+        public int course;
+        public string department;
+        public int group;
+        public string city;
+        int age;
+        // Создаем конструктор
+        public Student(string firstName, string lastName, string university, string faculty, string department, int course, int age, int group, string city)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.university = university;
+            this.faculty = faculty;
+            this.department = department;
+            this.course = course;
+            this.age = age;
+            this.group = group;
+            this.city = city;
+        }
+    }
+
+    class Program
+    {
+        static int MyDelegat(Student st1, Student st2)          // Создаем метод для сравнения для экземпляров
+        {
+            return String.Compare(st1.firstName, st2.firstName);          // Сравниваем две строки
+        }
+        static void Main(string[] args)
+        {
+            
+            int bakalavr = 0;
+            int magistr = 0;
+            int ages = 0;
+            int[] kurs;
+            List<Student> list = new List<Student>();                             // Создаем список студентов
+            DateTime dt = DateTime.Now;
+            StreamReader sr = new StreamReader("students_1.csv");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+                        // Добавляем в список новый экземпляр класса Student
+                    list.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                        // Одновременно подсчитываем количество бакалавров и магистров
+                    if (int.Parse(s[6]) < 5) bakalavr++; else magistr++;
+                    if (int.Parse(s[5]) > 17 && int.Parse(s[5]) < 21) ages++;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Ошибка!ESC - прекратить выполнение программы");
+                        // Выход из Main
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+                }
+            }
+            sr.Close();
+            list.Sort(new Comparison<Student>(MyDelegat));
+            Console.WriteLine("Всего студентов:" + list.Count);
+            Console.WriteLine("Магистров:{0}", magistr);
+            Console.WriteLine("Бакалавров:{0}", bakalavr);
+            Console.WriteLine("Студентов от 18 до 20 лет:{0}", ages);
+            foreach (var v in list) Console.WriteLine(v.firstName);
+            Console.WriteLine(DateTime.Now - dt);
+            Console.ReadKey();
+        }
+    }
+}
